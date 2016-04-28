2c20e97e2078f56fb2079c5157c2d7bf6871c60b
robbykraft <robbykraft@gmail.com>
2016-04-28 17:05:23 -0400

    conics looking like electric sheep

--- venice9-conics/src/ofApp.cpp
+++ venice9-conics/src/ofApp.cpp
@@ -33,12 +33,16 @@ void ofApp::draw(){
     static float PHI_ANGLE = 0.618 * M_PI;
 
     for(int i = 0 ;i < NUM_CONES; i++){
-        conics[i].setLookAt( ofVec3f(cosf(ofGetElapsedTimef()*.8), sinf(ofGetElapsedTimef()*.66), 1) );
-        plane = ofVec3f(60*powf((cosf(ofGetElapsedTimef()*.4444 + i*.4)), 5),
-                        60*powf((sinf(ofGetElapsedTimef()*.55555 + i*.4)), 5),
-                        100 + 100 * sinf(ofGetElapsedTimef()) );
-        planeNormal = ofVec3f(50 * cosf(ofGetElapsedTimef()+i*.1),
-                              50 * sinf(ofGetElapsedTimef()+i*.1),
+        float diff = powf(sinf(ofGetElapsedTimef()*.2222), 2);
+        float diff2 = powf(sinf(ofGetElapsedTimef()*.487), 2);
+        conics[i].setLookAt( ofVec3f(cosf(ofGetElapsedTimef()*.8 + i*.3)*diff,
+                                     sinf(ofGetElapsedTimef()*.66 + i*.3)*diff,
+                                     1) );
+        plane = ofVec3f(60*powf((cosf(ofGetElapsedTimef()*.4444 + i*.4)), 5)*diff,
+                        60*powf((sinf(ofGetElapsedTimef()*.55555 + i*.4)), 5)*diff,
+                        20);
+        planeNormal = ofVec3f(50 * cosf(ofGetElapsedTimef()+i*.1) * diff2,
+                              50 * sinf(ofGetElapsedTimef()+i*.1) * diff2,
                               100+ 80 * sinf(ofGetElapsedTimef() + 1/(i*PHI_ANGLE)) );
         conics[i].drawIntersectionsWithPlane(plane, planeNormal);
     }
